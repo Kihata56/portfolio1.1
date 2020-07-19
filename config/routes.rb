62cead2222fd7552da80207703cf7236d462605e
Kihata Syunpei <paypay569899@gmail.com>
2020-07-19 17:27:38 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'tweets#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'tweets#index'
   resources :tweets
+  resources :users, only: :show
 end
 
 
